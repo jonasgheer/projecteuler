d2cdbf595c76076353ed3e68b1c3731d3162d40f
Jonas Heer <jonas.g.heer@gmail.com>
2018-09-01 19:01:06 +0200

    done, accepts any number og digits

---
+++ problems/prob_4.py
@@ -0,0 +1,24 @@
+""" Largest palindrome product """
+
+def is_pal(n):
+    n = str(n)
+    if n == n[::-1]:
+        return True
+    else:
+        return False
+
+def lpp(n_d=3):
+    """ 
+        Find largest palindrome product 
+        n_d : number of digits 
+    """
+    palindromes = []
+    m = 10**n_d 
+    for i in range(1, m):
+        for j in range(1, m):
+            if is_pal(i*j):
+                palindromes.append(i*j)
+
+    return max(palindromes)
+    
+print('Largest palindrome product:', lpp())
