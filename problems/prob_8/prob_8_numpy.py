e823aa32aecd63d079600b03520ce0c2c452474b
Jonas Heer <jonas.g.heer@gmail.com>
2018-09-08 17:42:22 +0200

    done

--- problems/prob_8/prob_8_numpy.py
+++ problems/prob_8/prob_8_numpy.py
@@ -1,8 +1,25 @@
 """ Largest product in a series (numpy) """
 
-import numpy
+
+def slice_prod(slc):
+    """ Calculate product of slice """
+    prod = 1
+    for n in slc:
+        prod *= n
+    return prod
+
 
 with open('data.txt') as file:
-    data = file.read()
+    str_data = file.read()
+    data = [int(n) for n in str_data.replace('\n', '')]
 
-print(data)
+
+max_slice_prod = slice_prod(data[:13])  # Initial max
+
+for n in range(len(data)-13):
+    cur_slice_prod = slice_prod(data[n:n+13])
+    if cur_slice_prod > max_slice_prod:
+        max_slice_prod = cur_slice_prod
+
+
+print('Max 13 digit product:', max_slice_prod)
